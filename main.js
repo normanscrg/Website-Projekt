91e227c76d331d877e76d51a2536400d0e73fc09
Norman Scheurig <norman.scheurig@gmail.com>
2023-12-29 22:57:13 +0100

    footer/giffinder update 27_12

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const generateMemeBtn = document.querySelector(".meme_generator .generate_meme_btn");
-    const memeImage = document.querySelector(".meme_generator img");
-    const memeTitle = document.querySelector(".meme_generator .meme_title");
-    const memeAuthor = document.querySelector(".meme_generator .meme_author");
-    const buttonGym = document.getElementById("buttonGym");
-    const buttonHistory = document.getElementById("buttonHistory");
-    const buttonRandom = document.getElementById("buttonRandom");
+ 
+ 
+// Meme-Generator API
+  // Elemente abrufen
+  const generateMemeBtn = document.querySelector(".meme_generator .generate_meme_btn");
+  const memeImage = document.querySelector(".meme_generator img");
+  const memeTitle = document.querySelector(".meme_generator .meme_title");
+  const memeAuthor = document.querySelector(".meme_generator .meme_author");
+  const buttonGym = document.getElementById("buttonGym");
+  const buttonHistory = document.getElementById("buttonHistory");
+  const buttonRandom = document.getElementById("buttonRandom");
 
+  // Basis-URL für die Meme Generator API
+  let baseUrl = "https://meme-api.com/gimme/";
 
+  // Funktion zum Aktualisieren von Details
+  const updateDetails = (url, title, author) => {
+      memeImage.setAttribute("src", url);
+      memeTitle.innerHTML = title;
+      memeAuthor.innerHTML = `Meme by: ${author}`;
+  };
 
+  // Funktion zum Generieren von Memes
+  const generateMeme = (memeType) => {
+      const apiUrl = baseUrl + memeType;
 
+      fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+              updateDetails(data.url, data.title, data.author);
+          })
+          .catch((error) => console.error("Fehler beim Abrufen des Memes:", error));
+  };
 
-
-    // Basis-URL für die Meme-API
-    let baseUrl = "https://meme-api.com/gimme/";
-
-    const updateDetails = (url, title, author) => {
-        memeImage.setAttribute("src", url);
-        memeTitle.innerHTML = title;
-        memeAuthor.innerHTML = `Meme by: ${author}`;
-    };
-
-    const generateMeme = (memeType) => {
-
-        const apiUrl = baseUrl + memeType;
-
-        fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                updateDetails(data.url, data.title, data.author);
-            })
-            .catch((error) => console.error("Fehler beim Abrufen des Memes:", error));
-    };
-
-
-    document.querySelector(".meme_generator").addEventListener("click", function (event) {
+  // Event Listener für den Meme-Generator über verschiedene Buttons
+  document.querySelector(".meme_generator").addEventListener("click", function (event) {
       if (event.target === buttonRandom) {
           generateMeme("wholesomememes");
       } else if (event.target === buttonGym) {
@@ -43,8 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
           generateMeme("HistoryMemes");
       }
   });
-  
-   
 });
 
 
@@ -54,24 +52,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
+// Giphy API
 let APIKEY = "p31kaZjo3FtvNn6xgmVmsPAIKuuzrtYc";
-      // you will need to get your own API KEY
-      // https://developers.giphy.com/dashboard/
-      document.addEventListener("DOMContentLoaded", init);
-      function init() {
-        document.getElementById("btnSearch").addEventListener("click", ev => {
-          ev.preventDefault(); //to stop the page reload
-          let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-          let str = document.getElementById("search").value.trim();
-          url = url.concat(str);
-          console.log(url);
-          fetch(url)
-            .then(response => response.json())
-            .then(content => {
-              //  data, pagination, meta
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  document.getElementById("btnSearch").addEventListener("click", ev => {
+      ev.preventDefault(); // Um das Neuladen der Seite zu verhindern
+      let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+      let str = document.getElementById("search").value.trim();
+      url = url.concat(str);
+      console.log(url);
+      fetch(url)
+          .then(response => response.json())
+          .then(content => {
               console.log(content.data);
               console.log("META", content.meta);
+
+              // Elemente/GIF erstellen und einfügen
               let fig = document.createElement("figure");
               let img = document.createElement("img");
               let fc = document.createElement("figcaption");
@@ -81,18 +79,14 @@ let APIKEY = "p31kaZjo3FtvNn6xgmVmsPAIKuuzrtYc";
               fig.appendChild(img);
               fig.appendChild(fc);
 
-
-              let pic = document.querySelector(".pic");         
+              let pic = document.querySelector(".pic");
               pic.insertAdjacentElement("afterbegin", fig);
-              
 
+              // Eingabefeld zurücksetzen
               document.querySelector("#search").value = "";
-            })
-            .catch(err => {
+          })
+          .catch(err => {
               console.error(err);
-            });
-        });
-      }
-
-
-      
+          });
+  });
+}
